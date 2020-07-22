acde31f9d0a2b913f504b50595f053a2b2d4fae1
ReginaF2012 <skritschy@gmail.com>
2020-07-22 15:03:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Your code here
+function mapToNegativize(array) {
+    let newArray = []
+    array.forEach(element => {
+        // this ~ inverts, it's called the NOT Bitwise operator
+        // https://www.w3schools.com/js/js_bitwise.asp
+        let newNumber = ~element + 1
+        newArray.push(newNumber)
+    })
+    return newArray
+}
+
+function mapToNoChange(array) {
+    const newArray = array
+    return newArray
+}
+
+function mapToDouble(array){
+    let newArray = []
+    array.forEach(element => {
+        let newElement = element * 2
+        newArray.push(newElement)
+    })
+    return newArray
+}
+
+function mapToSquare(array){
+    let newArray = []
+    array.forEach(element => {
+        let newElement = element * element
+        newArray.push(newElement)
+    })
+    return newArray
+}
+
+function reduceToTotal(array, startingPoint = 0){
+    let total = startingPoint
+    for ( let i = 0; i < array.length; i++) {
+        total += array[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(array){
+    let truthiness = true
+    for(let i = 0; i < array.length; i++){
+        if (!!array[i] === false){
+            truthiness = false
+        }
+    }
+    return truthiness
+}
+
+function reduceToAnyTrue(array){
+    let truthiness = false
+    for(let i = 0; i < array.length; i++){
+        if (!!array[i] === true){
+            truthiness = true
+        }
+    }
+    return truthiness
+}
